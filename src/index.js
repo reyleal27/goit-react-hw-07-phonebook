36fd5bf20a6d1232e8f0f1525416628a8faaf740
reyleal27 <reyvincentleal@gmail.com>
2024-04-25 17:13:05 +0800

    hw7

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='https://github.com/reyleal27/goit-react-hw-07-phonebook.git/'>
+       <App />
+    </BrowserRouter>
+   
   </React.StrictMode>
 );
